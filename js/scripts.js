a8600d5d021ad92905ce3baffdceb8b0b036a625
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-17 10:18:01 -0700

    allow user to select a pepperoni topping if they would like

--- js/scripts.js
+++ js/scripts.js
@@ -15,5 +15,7 @@ userName = prompt('Welcome, valued customer! What is your name?');
 $('#pizzaform').submit(function(event) {
   event.preventDefault();
   var size = $('#size option:selected').val();
-  alert(size);
+  var topping = $('input[name="topping"]:checked').val();
+  console.log(size);
+  console.log(topping);
 });
