37951a9c46e78aaa285b0cd2f592d9c67fd452ee
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-17 11:32:15 -0700

    current working pizza total is displayed to user, updated on each change

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
 // global
 var userName;
 
+// back end
+
 function pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = toppings;
 }
 
-// bank end
+var sizeCosts = {"personal": 7, "small": 8, "medium": 10, "large": 12, "extra large": 13, "Woah.": 15};
+var crustCosts = {"traditional": 0, "New York": 0.5, "Sicilian": 1};
+var toppingCosts = {
+  "bacon": 1.5,
+  "bleu cheese crumble": 1,
+  "broccoli": 0.5,
+  "four cheese blend": 0.5,
+  "hummus": 1.5,
+  "pepperoni": 1,
+  "pineapple": 0.5,
+  "sweet chili sauce": 0.5
+};
+
+pizza.prototype.cost = function() {
+  var cost = 0;
+  cost += sizeCosts[this.size];
+  cost += crustCosts[this.crust];
+  this.toppings.forEach(function(topping) {
+    cost += toppingCosts[topping];
+  });
+  return cost;
+}
 
 
 
@@ -27,4 +50,14 @@ $('#pizzaform').submit(function(event) {
   }).get();
   var recentlyCreatedPizza = new pizza(size, crust, toppings);
   console.log(recentlyCreatedPizza);
+});
+
+$('#pizzaform').change(function() {
+  var size = $('#size option:selected').val();
+  var crust = $('#crust option:selected').val();
+  var toppings = $('input[name="topping"]:checked').map(function() {
+    return $(this).val();
+  }).get();
+  var currentlyCreatingPizza = new pizza(size, crust, toppings);
+  $('#estimatedcost').text(currentlyCreatingPizza.cost());
 });
